eadf90f216424c0b429d9d21e1b475a670cc95fb
raayappan <siddhant_1901cb49@iitp.ac.in>
2021-10-27 23:06:54 +0530

    CS384 Tutorial 6

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -20,30 +20,30 @@ def regex_renamer():
         name_of_series ="Lucifer"
        
     if name_of_series=='Breaking Bad':
-        files_series= os.listdir(os.getcwd()+"\\wrong_srt\\"+name_of_series)
-        for series in files_series:
-            series_sub= re.split(' ',series)
-            info_series=re.split('[se]',series_sub[2])
+        f= os.listdir(os.getcwd()+"\\wrong_srt\\"+name_of_series)
+        for series in f:
+            bb_sub= re.split(' ',series)
+            info_bb=re.split('[se]',bb_sub[2])
             newpath=os.getcwd()
-            shutil.copyfile(os.getcwd()+"\\wrong_srt\\"+name_of_series+"\\"+series,newpath+"\\corrected_srt\\Breaking Bad\\Breaking Bad - Season "+str('{:' + '0' + '>' + str(season_padding) + '}').format(info_series[1])+" Episode "+str('{:' + '0' + '>' + str(episode_padding) + '}').format(info_series[2])+"."+re.split('\.',series_sub[3])[3]) 
+            shutil.copyfile(os.getcwd()+"\\wrong_srt\\"+name_of_series+"\\"+series,newpath+"\\corrected_srt\\Breaking Bad\\Breaking Bad - Season "+str('{:' + '0' + '>' + str(season_padding) + '}').format(info_bb[1])+" Episode "+str('{:' + '0' + '>' + str(episode_padding) + '}').format(info_bb[2])+"."+re.split('\.',bb_sub[3])[3]) 
     
     elif name_of_series=='Game of Thrones':
-        files_series= os.listdir(os.getcwd()+"\\wrong_srt\\"+name_of_series)
-        for series in files_series:
-            series_main= re.split('.WEB',series)
-            series_sub=re.split("-",series_main[0])
-            info_series=re.split('[x]',series_sub[1])
+        f= os.listdir(os.getcwd()+"\\wrong_srt\\"+name_of_series)
+        for series in f:
+            got_main= re.split('.WEB',series)
+            got_sub=re.split("-",got_main[0])
+            info_got=re.split('[x]',got_sub[1])
             newpath=os.getcwd()
-            shutil.copyfile(os.getcwd()+"\\wrong_srt\\"+name_of_series+"\\"+series,newpath+"\\corrected_srt\\Game of Thrones\\Game of Thrones - Season "+str('{:' + '0' + '>' + str(season_padding) + '}').format(info_series[0].strip())+" Episode "+str('{:' + '0' + '>' + str(episode_padding) + '}').format(info_series[1])+"-"+series_sub[2]+"."+re.split('\.',series_main[1])[4])          
+            shutil.copyfile(os.getcwd()+"\\wrong_srt\\"+name_of_series+"\\"+series,newpath+"\\corrected_srt\\Game of Thrones\\Game of Thrones - Season "+str('{:' + '0' + '>' + str(season_padding) + '}').format(info_got[0].strip())+" Episode "+str('{:' + '0' + '>' + str(episode_padding) + '}').format(info_got[1])+"-"+ got_sub[2] +"."+re.split('\.',got_main[1])[4])          
     
     elif name_of_series=='Lucifer':
-        files_series= os.listdir(os.getcwd()+"\\wrong_srt\\"+name_of_series)
-        for series in files_series:
-            series_main= re.split('.HDTV',series)
-            series_sub=re.split("-",series_main[0])
-            info_series=re.split('[x]',series_sub[1])
+        f= os.listdir(os.getcwd()+"\\wrong_srt\\"+name_of_series)
+        for series in f:
+            luc_main= re.split('.HDTV',series)
+            luc_sub=re.split("-",luc_main[0])
+            info_luc=re.split('[x]',luc_sub[1])
             newpath=os.getcwd()+"\\corrected_srt\\Lucifer\\Lucifer - Season "
-            shutil.copyfile(os.getcwd()+"\\wrong_srt\\"+name_of_series+"\\"+series,newpath+str('{:' + '0' + '>' + str(season_padding) + '}').format(info_series[0].strip())+" Episode "+str('{:' + '0' + '>' + str(episode_padding) + '}').format(info_series[1])+"-"+series_sub[2]+"."+re.split('\.',series_main[1])[3])          
+            shutil.copyfile(os.getcwd()+"\\wrong_srt\\"+name_of_series+"\\"+series,newpath+str('{:' + '0' + '>' + str(season_padding) + '}').format(info_luc[0].strip())+" Episode "+str('{:' + '0' + '>' + str(episode_padding) + '}').format(info_luc[1])+"-"+luc_sub[2]+"."+re.split('\.',luc_main[1])[3])          
 
 if os.path.isdir("corrected_srt") == 0:
     os.mkdir("corrected_srt")
